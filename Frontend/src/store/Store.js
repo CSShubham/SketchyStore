3270766cba0697c9f77b069ef7755ab30e16e5d0
CSShubham <raj079097@gmail.com>
2026-01-20 00:35:18 +0530

    profile reducer added

--- Frontend/src/store/Store.js
+++ Frontend/src/store/Store.js
@@ -4,6 +4,7 @@ import cartReducer from "../slice/CartSlice";
 import WishlistReducer from "../slice/Wishlist";
 import orderReducer from "../slice/OrderSlice";
 import authReducer from "../slice/AuthSlice";
+import profileReducer from "../slice/ProfileSlice";
 export const store = configureStore({
-  reducer: { products: productsReducer, cart: cartReducer ,  wishlist: WishlistReducer, order: orderReducer , auth : authReducer},
+  reducer: { products: productsReducer, cart: cartReducer ,  wishlist: WishlistReducer, order: orderReducer , auth : authReducer, profile: profileReducer},
 });
