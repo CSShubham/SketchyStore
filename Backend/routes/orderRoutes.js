122f38cf7789d2e207ccc0eb8d4726fdba0eaafe
CSShubham <raj079097@gmail.com>
2026-01-20 13:14:35 +0530

    order routes updated

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 router.use(isAuthenticatedUser);
 
-router.post("/new", createOrder);
+router.post("/new", createOrder); // /api/order/new
 router.get("/me", myOrders);
 router.get("/:id", getSingleOrder);
 
