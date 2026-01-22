3570205018f4ebd3a50a25924688b1f78fb329e0
CSShubham <raj079097@gmail.com>
2026-01-22 15:10:57 +0530

    fix:CORS issue

--- Backend/server.js
+++ Backend/server.js
@@ -14,12 +14,27 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "*",
-    credentials: true,
-  }));
+const allowedOrigins = [
+  "http://localhost:5173", // Dev frontend
+  "https://your-frontend.vercel.app", // Production frontend (later)
+];
 
-  // Logger only in development
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }),
+);
+
+// Logger only in development
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
