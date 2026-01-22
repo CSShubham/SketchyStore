c9da9b3c4135d5ecb2b93c1147ae2c453b135bf7
CSShubham <raj079097@gmail.com>
2026-01-22 15:28:40 +0530

    CORS lock fro deployment

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173", // Dev frontend
-  "https://your-frontend.vercel.app", // Production frontend (later)
+  "https://sketchystore-frontend.vercel.app", // Production frontend (later)
 ];
 
 app.use(
